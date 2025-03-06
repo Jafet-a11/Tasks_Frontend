086e3b47fab8debc3b84447632dddd0588d8ad99
Jafet-a11 <145033871+Jafet-a11@users.noreply.github.com>
2025-03-06 03:29:35 -0600

    Tasks_Frontend1

--- src/Rutas.js
+++ src/Rutas.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import DashboardPage from "./Pages/Dashboard/DashboardPage";
 import DashboardUser from "./Pages/Dashboard/DashboardUser";
 import DashboardGroups from "./Pages/Dashboard/DashboardGroups";
